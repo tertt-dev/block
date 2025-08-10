a80ccdab7ffe0509ca86acbe69f2ba2794e4429d
dev <tertt.dev@gmail.com>
2025-08-10 14:55:16 +0300

    fix

--- program/src/ix.rs
+++ program/src/ix.rs
@@ -8,6 +8,7 @@ pub enum RouterInstruction {
         quote_mint: Pubkey,
         amount_in: u64,
         min_amount_out: u64,
+        // carry DEX-specific ix data (ignored in program)
     }
 }
 
@@ -17,7 +18,7 @@ impl RouterInstruction {
         if data.len() < 1 { return Err(()); }
         match data[0] {
             0 => {
-                if data.len() != 1 + 32 + 32 + 8 + 8 { return Err(()); }
+                if data.len() < 1 + 32 + 32 + 8 + 8 { return Err(()); }
                 let token_mint = Pubkey::new_from_array(data[1..33].try_into().unwrap());
                 let quote_mint = Pubkey::new_from_array(data[33..65].try_into().unwrap());
                 let amount_in = u64::from_le_bytes(data[65..73].try_into().unwrap());
